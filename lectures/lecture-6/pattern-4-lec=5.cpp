654a6648fc3148317bb867dfe95de6ebdec27ae6
sagar25444x <sagar25444x@gmailcom>
2026-02-16 11:23:35 +0530

    notes

--- lectures/lecture-6/pattern-4-lec=5.cpp
+++ lectures/lecture-6/pattern-4-lec=5.cpp
@@ -7,10 +7,14 @@ int main()
   cin >> n;
   for (int i = 0; i <= n - 1; i++)
   {
+    for (int j = 0; j <= n - i; j++)
+    {
+      cout << "  ";
+    }
     for (int j = 0; j <= i; j++)
     {
       cout << "* ";
     }
-    cout << "\n";
+    cout << endl;
   }
 }
