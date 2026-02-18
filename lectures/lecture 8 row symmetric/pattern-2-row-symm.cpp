985e09bba470be3a914563ab7769b2d07ac0ac48
sagar25444x <sagar25444x@gmailcom>
2026-02-18 11:16:35 +0530

    jj

---
+++ lectures/lecture 8 row symmetric/pattern-2-row-symm.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+  int n;
+  cin >> n;
+  for (int i = 0; i < n ; i++)
+  {
+    for (int j = 0; j < n - i; j++)
+    {
+      cout << "  ";
+    }
+
+    for (int j = 0; j < i + 1; j++)
+    {
+      cout << "* " /* code */;
+    }
+    cout << endl;
+  }
+
+  // for a symmetric
+  for (int i = n; i > 0 ; i--)
+  {
+    for (int j = 0; j < n - i; j++)
+    {
+      cout << "  ";
+    }
+
+    for (int j = 0; j < i + 1; j++)
+    {
+      cout << "* " /* code */;
+    }
+    cout << endl;
+  }
+}
