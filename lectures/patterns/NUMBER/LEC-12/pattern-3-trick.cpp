663fe05e021bd9af9f5a3d92f93ba1751b0af100
sagar25444x <sagar25444x@gmailcom>
2026-02-27 10:49:31 +0530

    notes

---
+++ lectures/patterns/NUMBER/LEC-12/pattern-3-trick.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = n; j >= 1; j--)//column ulta cahl rha vide record
+    {
+      if (i == j)
+      {
+        cout<<"* ";
+      }
+      else{
+        cout<<j<<"  ";
+      }
+    }
+    cout<<endl;
+  }
+
+  return 0;
+}
